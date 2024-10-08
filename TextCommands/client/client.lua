011f5fccabd2041bd3e48befc4544e2e65759349
xLukazz7 <156589485+xLukazz7@users.noreply.github.com>
2024-10-08 17:20:43 +0200

    Add files via upload

---
+++ TextCommands/client/client.lua
@@ -0,0 +1,17 @@
+Registercommand('serverinfo', function (source, args)
+    TriggerEvent('chat:addMessage', {
+        args = {'Dit is een Dev Server, dus er is nog geen info'}
+    })
+end);
+
+RegisterCommand('carsinfo', function (source, args)
+    TriggerEvent('chat:addMessage', {
+        args = {'De cars die hier in zitten zijn lore friendly.(GTA5 cars dus)'}
+    })
+end);
+
+RegisterCommand('ToDO', function (source, args)
+    TriggerEvent('chat:addMessage', {
+        args = {'*/car command maar dan voor helikopters. *X Y Z Coordinaten met /getcoords'}
+    })
+end);
